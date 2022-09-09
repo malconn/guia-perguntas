90d2a7fef9ba05e18af83ba2f4bfab0296fe1510
Malcon Augusto Souza de Miranda <malcon.miranda@nm.local>
2022-09-09 07:42:22 -0300

    apresentando e instalando o bootstrap

--- index.js
+++ index.js
@@ -5,26 +5,10 @@ const app = express();
 app.set('view engine','ejs');
 app.use(express.static("public"));
 
-app.get('/:nome/:lang',(req,res)=>{
- const nome = req.params.nome;
- const lang = req.params.lang;
- const exibirMsg = true;
- const produtos = [
-  {nome:'Doritos',preco:3.14},
-  {nome:'Coca-cola',preco:5},
-  {nome:'Carne',preco:15},
-  {nome:'Redbull',preco:6},
-  {nome:'Leite',preco:1.45}
- ]
+app.get('/',(req,res)=>{
 
 
- res.render('index',{
-  nome:nome,
-  lang:lang,
-  empresa:'Malcon Dev',
-  msg: exibirMsg,
-  produtos:produtos
- });
+ res.render('index');
 });
 
 app.listen(8080,()=>{
