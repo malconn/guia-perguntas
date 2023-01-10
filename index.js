7753bca56b37b274552da8089249607fe7778059
malconn <malconaugusto@outlook.com>
2023-01-10 20:49:38 -0300

    feat: Ordenacao com Sequelize

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
- Pergunta.findAll({ raw: true }).then(questions =>{
+ Pergunta.findAll({ raw: true , order: [
+  ['id','DESC'] // ASC = Crescente || DESC = Decrescente
+ ]}).then(questions =>{
   res.render('index',{
    questions:questions
   });
